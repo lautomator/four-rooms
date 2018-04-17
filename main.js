75544ef2a1c43f66419c8bf7233c7130f98363f8
John Merigliano <john.merigliano@thefire.org>
2018-04-17 12:49:30 -0400

    added sword img; lint

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var fourRoomsApp = function (d) {
     /* Key options:
      * ArrowUp, ArrowRight, ArrowLeft, ArrowDown
      * 8 (up), 6 (right), 4 (left), 2 (down)
-     * u (up), k (right), h (left), n (down) */
+     * u (up), k (right), h (left), n (down)
+     * action: spacebar */
 
     var model = {
         htmlTargets: d.targets,
@@ -13,40 +14,14 @@ var fourRoomsApp = function (d) {
             down: ["ArrowDown", "2", "n"],
             left: ["ArrowLeft", "4", "h"],
             right: ["ArrowRight", "6", "k"],
-            action: ["Shift"]
+            action: 32 // spacebar
         },
         pos: {
             current: [25, 25], // x, y
             previous: [25, 25]
         },
         incr: 25,
-        map: [[0, 0], [25, 0], [50, 0], [75, 0], [100, 0], [125, 0], [150, 0], [175, 0], [200, 0], [225, 0], [250, 0], [275, 0], [300, 0], [325, 0], [350, 0], [375, 0], [400, 0], [425, 0], [450, 0], [475, 0], [500, 0], [525, 0], [550, 0], [575, 0], [600, 0], [600, 25], [600, 50], [600, 75], [600, 100], [600, 125], [600, 150], [600, 175], [600, 200], [600, 225], [600, 250], [600, 275], [600, 300], [600, 325], [600, 350], [600, 375], [600, 400], [600, 425], [600, 450], [600, 475], [600, 500], [600, 525], [600, 550], [600, 575], [600, 600], [575, 600], [550, 600], [525, 600], [500, 600], [475, 600], [450, 600], [425, 600], [400, 600], [375, 600], [350, 600], [325, 600], [300, 600], [275, 600], [250, 600], [225, 600], [200, 600], [175, 600], [150, 600], [125, 600], [100, 600], [75, 600], [50, 600], [25, 600], [0, 600], [0, 575], [0, 550], [0, 525], [0, 500], [0, 475], [0, 450], [0, 425], [0, 400], [0, 375], [0, 350], [0, 325], [0, 300], [0, 275], [0, 250], [0, 225], [0, 200], [0, 175], [0, 150], [0, 125], [0, 100], [0, 75], [0, 50], [0, 25], [450, 25], [450, 50], [450, 75], [450, 100], [450, 125], [450, 200], [25, 225], [50, 225], [75, 225], [100, 225], [125, 225], [150, 225], [175, 225], [200, 225], [225, 225], [250, 225], [250, 250], [250, 275], [250, 300], [250, 325], [250, 350], [250, 375], [250, 400], [250, 425], [250, 450], [250, 475], [250, 500], [275, 225], [250, 575], [350, 225], [375, 225], [400, 225], [425, 225], [450, 225], [475, 225], [500, 225], [525, 225], [550, 225], [575, 225]],
-        rooms: {
-            roomA: {
-                perim: {
-                    x: [25, 425], // min, max
-                    y: [25, 200]
-                }
-            },
-            roomB: {
-                perim: {
-                    x: [475, 575],
-                    y: [25, 200]
-                }
-            },
-            roomC: {
-                perim: {
-                    x: [25, 225],
-                    y: [225, 575]
-                }
-            },
-            roomD: {
-                perim: {
-                    x: [275, 575],
-                    y: [250, 575]
-                }
-            }
-        }
+        map: [[0, 0], [25, 0], [50, 0], [75, 0], [100, 0], [125, 0], [150, 0], [175, 0], [200, 0], [225, 0], [250, 0], [275, 0], [300, 0], [325, 0], [350, 0], [375, 0], [400, 0], [425, 0], [450, 0], [475, 0], [500, 0], [525, 0], [550, 0], [575, 0], [600, 0], [600, 25], [600, 50], [600, 75], [600, 100], [600, 125], [600, 150], [600, 175], [600, 200], [600, 225], [600, 250], [600, 275], [600, 300], [600, 325], [600, 350], [600, 375], [600, 400], [600, 425], [600, 450], [600, 475], [600, 500], [600, 525], [600, 550], [600, 575], [600, 600], [575, 600], [550, 600], [525, 600], [500, 600], [475, 600], [450, 600], [425, 600], [400, 600], [375, 600], [350, 600], [325, 600], [300, 600], [275, 600], [250, 600], [225, 600], [200, 600], [175, 600], [150, 600], [125, 600], [100, 600], [75, 600], [50, 600], [25, 600], [0, 600], [0, 575], [0, 550], [0, 525], [0, 500], [0, 475], [0, 450], [0, 425], [0, 400], [0, 375], [0, 350], [0, 325], [0, 300], [0, 275], [0, 250], [0, 225], [0, 200], [0, 175], [0, 150], [0, 125], [0, 100], [0, 75], [0, 50], [0, 25], [450, 25], [450, 50], [450, 75], [450, 100], [450, 125], [450, 200], [25, 225], [50, 225], [75, 225], [100, 225], [125, 225], [150, 225], [175, 225], [200, 225], [225, 225], [250, 225], [250, 250], [250, 275], [250, 300], [250, 325], [250, 350], [250, 375], [250, 400], [250, 425], [250, 450], [250, 475], [250, 500], [275, 225], [250, 575], [350, 225], [375, 225], [400, 225], [425, 225], [450, 225], [475, 225], [500, 225], [525, 225], [550, 225], [575, 225]]
     };
 
     function checkMap(pos) {
@@ -71,9 +46,11 @@ var fourRoomsApp = function (d) {
     }
 
     function movePointer(k) {
-        // Takes in the key name <str>.
+        // Takes in the key hit <array>.
         // Returns the new position <array>.
         const incr = model.incr;
+        var keyName = k[0];
+        var keyCode = k[1];
         var x = model.pos.current[0];
         var y = model.pos.current[1];
         var pos = {
@@ -83,16 +60,16 @@ var fourRoomsApp = function (d) {
 
         pos.previous = [x, y];
 
-        if (model.actionKeys.up.indexOf(k) > -1) {
+        if (model.actionKeys.up.indexOf(keyName) > -1) {
             y -= incr; // up
-        } else if (model.actionKeys.right.indexOf(k) > -1) {
+        } else if (model.actionKeys.right.indexOf(keyName) > -1) {
             x += incr; // right
-        } else if (model.actionKeys.left.indexOf(k) > -1) {
+        } else if (model.actionKeys.left.indexOf(keyName) > -1) {
             x -= incr; // left
-        } else if (model.actionKeys.down.indexOf(k) > -1) {
+        } else if (model.actionKeys.down.indexOf(keyName) > -1) {
             y += incr; // down
         } else {
-            if (k === model.actionKeys.action[0]) {
+            if (keyCode === model.actionKeys.action) {
                 // action
                 console.log("action");
             }
@@ -113,7 +90,6 @@ var fourRoomsApp = function (d) {
     // views
     function renderPointer(pos) {
         var pointerEl = model.htmlTargets.pointer;
-
         pointerEl.style.left = pos[0].toString() + "px";
         pointerEl.style.top = pos[1].toString() + "px";
     }
@@ -122,19 +98,13 @@ var fourRoomsApp = function (d) {
         renderPointer(m.pos.current);
 
         document.addEventListener("keydown", function (item) {
-            var keyName = item.key;
-            var pos = movePointer(keyName);
-
+            var keyHit = [item.key, item.keyCode];
+            var pos = movePointer(keyHit);
             model.pos.current = pos.current;
             model.pos.previous = pos.previous;
-            // console.log("pos:", m.pos.current, "prev", m.pos.previous);
             renderPointer(m.pos.current);
-        });
-
-        document.addEventListener("keyup", function (item) {
             console.log("pos:", model.pos.current, "prev", model.pos.previous);
         });
-
     }
     main(model);
 };
